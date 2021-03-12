ed983191f32f0a3d6c02f07b975076f393867d9e
tha <tha@aperia.com>
2021-03-12 14:52:06 -0600

    Adding customer.Payment Method Code

--- AdventureWorksLT2012/AdventureWorksLT2012/SalesLT/Tables/Customer.sql
+++ AdventureWorksLT2012/AdventureWorksLT2012/SalesLT/Tables/Customer.sql
@@ -11,6 +11,7 @@
     [EmailAddress] NVARCHAR (50)     NULL,
     [Phone]        [dbo].[Phone]     NULL,
     [Language Code] NVARCHAR(10)     NULL,
+    [Shipping Method Code] NVARCHAR(10)     NULL,
     [PasswordHash] VARCHAR (128)     NOT NULL,
     [PasswordSalt] VARCHAR (10)      NOT NULL,
     [rowguid]      UNIQUEIDENTIFIER  CONSTRAINT [DF_Customer_rowguid] DEFAULT (newid()) ROWGUIDCOL NOT NULL,
