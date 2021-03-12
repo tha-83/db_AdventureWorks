52bbf37dd513852767f9b948ae212a2f1e1ec44d
tha <tha@aperia.com>
2021-03-12 12:21:25 -0600

    Init database objects

---
+++ AdventureWorksLT2012/AdventureWorksLT2012/SalesLT/Tables/Customer_temp.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [SalesLT].[Customer_temp] (
+    [CustomerID]   INT               NOT NULL,
+    [NameStyle]    [dbo].[NameStyle] NOT NULL,
+    [Title]        NVARCHAR (8)      NULL,
+    [FirstName]    [dbo].[Name]      NOT NULL,
+    [MiddleName]   [dbo].[Name]      NULL,
+    [LastName]     [dbo].[Name]      NOT NULL,
+    [Suffix]       NVARCHAR (10)     NULL,
+    [CompanyName]  NVARCHAR (128)    NULL,
+    [SalesPerson]  NVARCHAR (256)    NULL,
+    [EmailAddress] NVARCHAR (50)     NULL,
+    [Phone]        [dbo].[Phone]     NULL,
+    [PasswordHash] VARCHAR (128)     NOT NULL,
+    [PasswordSalt] VARCHAR (10)      NOT NULL,
+    [rowguid]      UNIQUEIDENTIFIER  ROWGUIDCOL NOT NULL,
+    [ModifiedDate] DATETIME          NOT NULL
+);
+
